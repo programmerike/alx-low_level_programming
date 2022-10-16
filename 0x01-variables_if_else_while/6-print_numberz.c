f8c0577e8a426cefa5ad438052fac55db89cd7ad
programmerike <isaac1700mensah@gmail.com>
2022-10-16 07:56:42 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int numbs;
 
-	for (numbs = 0; numbs <= 10; numbs++)
-		putchar((numbs % 10)  + 0);
+	for (numbs = 0; numbs < 10; numbs++)
+		putchar((numbs % 10)  + '0');
 	putchar('\n');
 
 	return (0);
