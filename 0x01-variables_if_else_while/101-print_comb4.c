17b65bdbb3fd188a1a78aa24ba75f8b1fbfd7b39
programmerike <isaac1700mensah@gmail.com>
2022-10-17 16:56:18 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int a, b, c;
+for (a = 0; a < 8; a++)
+{
+for (b = a + 1; b < 9; b++)
+{
+for (c = b + 1; c < 10; c++)
+{
+putchar((a % 10) + '0');
+putchar((b % 10) + '0');
+putchar((c % 10) + '0');
+
+if (a == 7 && b == 8 && c == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+}
