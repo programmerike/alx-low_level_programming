88fa236d705ba2fe1f200e037ad807470f28349d
programmerike <isaac1700mensah@gmail.com>
2022-10-17 16:24:26 -0700

    Combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,12 @@
 int main(void)
 {
 int x, y;
-
 for (x = 0; x <= 9; x++)
 {
 for (y = x + 1; y <= 9; y++)
 {
     putchar(x + '0');
     putchar(y + '0');
-
     if (x < 8)
 {
       putchar(',');
@@ -22,8 +20,6 @@ for (y = x + 1; y <= 9; y++)
     }
   }
 }
-
   putchar('\n');
-
   return (0);
 }
