7cc868c97f060a927170da5dbd47e1e138e0fb5d
programmerike <isaac1700mensah@gmail.com>
2022-10-17 15:46:30 -0700

    Combo

---
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - print combination of two numbers
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+
+for (x = 0; x <= 9; x++)
+{
+for (y = x + 1; y <=9; y++)
+{
+    putchar(x + '0');
+    putchar(y + '0');
+
+    if (x < 8)
+    {
+      putchar(',');
+      putchar(' ');
+    }
+  }
+}
+
+  putchar('\n');
+
+  return (0);
+}
