946a6891944db4dd4837bdd5ab93165fbf9c1e6d
programmerike <isaac1700mensah@gmail.com>
2022-10-20 13:31:56 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,19 +10,18 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-largest = a, b, c;
+	
 	if (a > b && b > c)
 	{
-		printf("The largest number is a\n");
+		largest = a;
 	}
 	else if (b > a && a > c)
 	{
-		printf("The largest number is b\n");
+		largest = b;
 	}
 	else
 	{
-		printf("The largest number is c\n");
+		largest = c;
 	}
-
 	return (largest);
 }
