dd78753930d199b52364d09544955d978cac461c
programmerike <isaac1700mensah@gmail.com>
2022-10-20 04:35:00 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,2 +1,30 @@
-#!/bin/bash
-gcc -E $CFILE -o  c
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	largest = a, b, c;
+
+	if (a > b && b > c)
+	{
+		printf("The largest number is %d", a);
+	}
+	else if (b > a && a > c)
+	{
+		printf("The largest number is %d", b);
+	}
+	else
+	{
+		printf("The largest number is %d", c);
+	}
+
+	return (largest);
+}
